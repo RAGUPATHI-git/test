c81d950f0d74ca4b49fa134e13bf5453fcb454be
professor ragu <ragu46270@gmail.com>
2025-02-21 22:57:45 +0530

    "folder structure changed by ragu"

--- lib/core/routing/app_routing.dart
+++ lib/core/routing/app_routing.dart
@@ -1,19 +1,16 @@
-import 'package:demo3/presentation/screens/home_screen.dart';
-import 'package:demo3/presentation/screens/error/page_404.dart';
-import 'package:demo3/presentation/screens/user%20screens/notification_screen.dart';
+
+import 'package:demo3/features/auth/presentation/pages/login_screen.dart';
 import 'package:flutter/material.dart';
 
 class AppRouting {
   Route onRouteGenerator(RouteSettings route) {
     switch (route.name) {
       case '/':
-        return MaterialPageRoute(builder: (context) => const HomeScreen());
+        return MaterialPageRoute(builder: (context) => const LoginScreen());
 
-      case '/notification':
-        return MaterialPageRoute(builder: (context) => NotificationScreen());
 
       default:
-        return MaterialPageRoute(builder: (conext) => const Page404());
+        return MaterialPageRoute(builder: (conext) => const LoginScreen());
     }
   }
 }
